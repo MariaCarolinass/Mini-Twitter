55b8cf1c3b7b9077dfadabbf2d7578598c5d0760
mariacarolinass <m.carolina.soares1@gmail.com>
2022-01-19 00:24:18 -0300

    add funções de hash de senha

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -24,6 +25,12 @@ class User(db.Model):
             'email': self.email,
             'password_hash': self.password_hash
         }
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     followed = db.relationship(
         'User', secondary=followers,
