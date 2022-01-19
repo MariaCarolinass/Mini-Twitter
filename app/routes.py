6874877f66148097bfa72bd8ba70cf73f47a7f0d
mariacarolinass <m.carolina.soares1@gmail.com>
2022-01-19 13:33:00 -0300

    implementei rotas de login do user

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,32 @@
+from flask import render_template, flash, url_for, redirect
 from app import app
+from flask_login import current_user, login_user, login_required
+from app.models import User, Post
+from app.forms import LoginForm
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Início')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('E-mail ou senha inválido')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Entrar', form=form)
