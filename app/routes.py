7a8a36562ee4d5d80d31ec9acaa2437bcfa61e7e
mariacarolinass <m.carolina.soares1@gmail.com>
2022-01-19 16:56:07 -0300

    implementei cadastro de usuario

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, flash, url_for, redirect
-from app import app
-from flask_login import current_user, login_user, login_required
+from app import app, db
+from flask import render_template, flash, url_for, request, redirect
+from flask_login import current_user, login_user, login_required, logout_user
+from werkzeug.urls import url_parse
 from app.models import User, Post
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
@@ -30,3 +31,18 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Entrar', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, nickname=form.nickname.data,
+            email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Parabéns, seu usuário foi cadastrado!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Cadastre-se', form=form)
