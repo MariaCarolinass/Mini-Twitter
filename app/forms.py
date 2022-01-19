b5b3a35a8b969a8dcf20557a2c91c2e1eddc19b9
mariacarolinass <m.carolina.soares1@gmail.com>
2022-01-19 16:56:52 -0300

    add form de cadastro de usuario

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User, Post
 
 class LoginForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired()])
     password = PasswordField('Senha', validators=[DataRequired()])
     remember_me = BooleanField('Lembre de mim')
     submit = SubmitField('Entrar')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Nome', validators=[DataRequired()])
+    nickname = StringField('Apelido', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    password2 = PasswordField('Repetir senha', validators=[DataRequired(),
+        EqualTo('password')])
+    submit = SubmitField('Cadastrar')
+
+    def validate_nickname(self, nickname):
+        user = User.query.filter_by(nickname=nickname.data).first()
+        if user is not None:
+            raise ValidationError('Por favor use um apelido diferente.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Por favor use um endereço de e-mail diferente.')
