29ab649415da6b725e24a885ef5c5e19b55b7bcc
mariacarolinass <m.carolina.soares1@gmail.com>
2022-01-19 13:33:40 -0300

    add implementacao de login

--- app/models.py
+++ app/models.py
@@ -1,12 +1,17 @@
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), index=True)
     nickname = db.Column(db.String(70), index=True, unique=True)
