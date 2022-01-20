52198c0cbb6a22c179013f498a65e2398db420e9
mariacarolinass <m.carolina.soares1@gmail.com>
2022-01-20 16:41:17 -0300

    add importação do pydantic

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_pydantic_spec import FlaskPydanticSpec
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+spec = FlaskPydanticSpec('flask', title='Mini-Twitter')
+spec.register(app)
 
 from app import routes, models
