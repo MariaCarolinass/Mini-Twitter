604f110bad854f159449e803a80e4dd842b4d55a
mariacarolinass <m.carolina.soares1@gmail.com>
2022-01-20 20:34:00 -0300

    implementei funções p/ seguir e parar de seguir usuario

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ followers = db.Table('followers',
 def load_user(id):
     return User.query.get(int(id))
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), index=True)
@@ -19,8 +20,14 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(130))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
     def __repr__(self):
-        return '<Usuario {}>'.format(self.username)
+        return '{}'.format(self.username)
 
     def as_dict(self):
         return {
@@ -36,11 +43,25 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    followed = db.relationship(
-        'User', secondary=followers,
-        primaryjoin=(followers.c.follower_id == id),
-        secondaryjoin=(followers.c.followed_id == id),
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -48,7 +69,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Postagem {}>'.format(self.message)
+        return '{}'.format(self.message)
 
     def as_dict(self):
         return {
