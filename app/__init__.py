09d92d5637e46330d8b5e090a205a44189751bdf
mariacarolinass <m.carolina.soares1@gmail.com>
2022-01-20 22:43:51 -0300

    atualizei paginas com o bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 from flask_pydantic_spec import FlaskPydanticSpec
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+bootstrap = Bootstrap(app)
 spec = FlaskPydanticSpec('flask', title='Mini-Twitter')
 spec.register(app)
 
