365a029531605e08f5d1dec279682043c945994d
mariacarolinass <m.carolina.soares1@gmail.com>
2022-01-21 02:00:01 -0300

    add paginacao e pagina explorar

--- app/routes.py
+++ app/routes.py
@@ -38,8 +38,23 @@ def index():
         db.session.commit()
         flash('Parabéns, sua postagem foi cadastrada!')
         return redirect(url_for('index'))
-    posts = Post.query.all()
-    return render_template('index.html', title='Início', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Início', form=form,
+        posts=posts.items, next_url=next_url, prev_url=prev_url)
+
+#Encontrar mais postagens
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('explore.html', title='Explorar postagens',
+        posts=posts)
 
 #Funcionalidades de login e cadastro do usuário
 @app.route('/logout')
